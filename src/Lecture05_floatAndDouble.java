e3c26416168d54d1c66f62fbb83b7b6c629eb711
Mahamud Hasan <fmhasan25@gmail.com>
2020-11-20 15:19:44 -0500

    committing challenges_Primitive

--- src/Lecture05_floatAndDouble.java
+++ src/Lecture05_floatAndDouble.java
@@ -1,4 +1,4 @@
-public class floatAndDouble {
+public class Lecture05_floatAndDouble {
 
     public static void main(String[] args) {
 
---
+++ src/Lecture06_charAndBoolean.java
@@ -0,0 +1,35 @@
+public class Lecture06_charAndBoolean {
+    public static void main(String[] args) {
+
+        /**
+         * A char occupies two bytes of memory or 16 bits and thus has a width of 16. The reason it's
+         * not just a single byte is that it allows you to store unicode characters
+         *
+         * unicode: unicode is an international encoding standard for use with different languages and scripts
+         * by which each letter, digit or symbol is assigned a unique numeric value that applies across
+         * different platforms and programs.
+         * 2 bytes and capable to represent 65535 different types
+         * browser https://unicode-table.com
+         * */
+
+        char myChar = 'D';
+        char myUniCodeChar = '\u0044';
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myChar);
+        System.out.println(myUniCodeChar);
+        System.out.println(myCopyrightChar);
+
+
+        /**
+         * Boolean works with true or false
+         * */
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        boolean isCustomerOver21 = true;
+
+
+
+
+    }//end of the main method
+}//end of the java class
