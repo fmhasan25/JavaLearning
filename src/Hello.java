1cf318a4135055ba0018a7db904b3a7dd5708bd7
Mahamud Hasan <fmhasan25@gmail.com>
2020-11-20 10:43:57 -0500

    second commit

---
+++ src/Hello.java
@@ -0,0 +1,22 @@
+public class Hello {
+    /**
+     * public/private are access modifier
+     * {} are class body
+     * Method are collection of statements that performs an operation
+     */
+    public static void main(String[] args) {
+        /**
+         * public is the static modifier
+         * static is a java keyword that needs an understanding of other concepts
+         * void is yet another keyword used to indicate that the method will not return anything more
+         * on that later
+         * () method declaration or what will be used within the method
+         * {} code block
+         * 'Hello World' is the statement which will be executed
+        */
+
+        System.out.println("Hello World");
+
+    }//end of the main method
+
+}//end of the java class
