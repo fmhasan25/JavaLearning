325fd8ce168f940144ee3a9c131cd0b3558dabaa
Mahamud Hasan <fmhasan25@gmail.com>
2020-11-20 15:03:52 -0500

    committing challenges_Primitive

---
+++ src/challenge02_floatAndDouble.java
@@ -0,0 +1,21 @@
+public class challenge02_floatAndDouble {
+    public static void main(String[] args) {
+
+        /**
+         * create a variable for weight in pound which to be converted to kg
+         * calculate the result
+         * print the result
+         * */
+        double qtyInKg= 200d;
+        double unitWeightLb = 0.45359237d;
+        double unitWeightKg = qtyInKg * unitWeightLb;
+        System.out.println(unitWeightKg);
+
+        /**
+         * representing PI value in java
+         * */
+
+        final double pi = 3.1415987d;
+
+    }
+}
