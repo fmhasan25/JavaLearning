8e465f21aaf096291d6285cfd9d88ecd4861d5f3
Mahamud Hasan <fmhasan25@gmail.com>
2020-11-20 14:47:29 -0500

    committing challenges_Primitive

---
+++ src/floatAndDouble.java
@@ -0,0 +1,24 @@
+public class floatAndDouble {
+
+    public static void main(String[] args) {
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = "+myMinFloatValue);
+        System.out.println("Float minimum value = "+myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = "+myMinDoubleValue);
+        System.out.println("Double minimum value = "+myMaxDoubleValue);
+
+
+        int myIntValue = 5;
+        float myFloatValue = 5.25f;
+        double myDoubleValue = 5.25d;
+
+
+
+    }//end of the main method
+}//end of the java class
+
