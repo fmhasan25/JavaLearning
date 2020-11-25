54d290834c94d9d4ec0d97d8dc41779b786562b1
Mahamud Hasan <fmhasan25@gmail.com>
2020-11-24 22:44:37 -0500

    Added the lecture 08

---
+++ src/Lecture08_operator.java
@@ -0,0 +1,17 @@
+public class Lecture08_operator {
+
+    public static void main(String[] args) {
+        int result = 1 + 2; //1+2=3
+        int a = 2;
+        int b = 4;
+        System.out.println(result);
+        System.out.println(a+b);
+        result = result *10;
+        System.out.println("result * 10 = "+result);
+        result = result/2;
+        System.out.println("result / 2 ="+ result);
+        result = result % 4;
+        System.out.println("result % 4 = "+ result);
+
+    }
+}
