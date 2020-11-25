00c5be1a71f18eee2894e56eaf62cd0440f9067d
Mahamud Hasan <fmhasan25@gmail.com>
2020-11-24 23:08:38 -0500

    Added the lecture 08

---
+++ src/Lecture09_keywordsAndExpression.java
@@ -0,0 +1,20 @@
+public class Lecture09_keywordsAndExpression {
+
+    public static void main(String[] args) {
+        /**
+         * int part is the keyword and rest are expression
+         * mile = 1.609344 km
+         * */
+        int int2= 5;
+        //double is the keyword and rest are expression
+        double kilometres = (100 * 1.609344);
+        int highScore = 50;
+
+        if(highScore==50){
+            System.out.println("This is an expression");
+        }
+
+
+
+    }//end of the main method
+}//end of the java class
